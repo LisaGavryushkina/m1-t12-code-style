d467a5b80d8c4a54862f87b5f093133b2d6b2b8f
Stanislav Klimov <stanklimov@yandex.ru>
2022-12-24 23:07:30 +0300

    Update DepositCalculator.java
    Добавил комментарии

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 
 public class DepositCalculator {
     //не знаю, нужно ли что-то делать с названиями этих методов?
+    // лучше не делать переносов строк, если сигнатура до фигурной скобки влезает в 120 символов
     double calculateComplexPercent(double amount, double yearRate,
                                    int depositPeriod) {
         double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
         return round(pay, 2);
     }
 
+    // лучше не делать переносов строк, если сигнатура до фигурной скобки влезает в 120 символов
     double calculateSimplePercent(double amount, double yearRate,
                                   int depositPeriod) {
+        // тут сложно разобраться в формуле расчета из-за переноса, лучше все в одну строку оставить
         return round(amount + amount * yearRate
                 * depositPeriod, 2);
     }
@@ -25,14 +28,18 @@ public class DepositCalculator {
         int action;
         Scanner scanner = new Scanner(System.in);
 
+        // я обычно разбиваю строки с выводом приглашающего текста и сканнером в отдельные блоки, так понятнее, но вроде это не противоречит стайлу
         System.out.println("Введите сумму вклада в рублях:");
+        // переменную лучше вынести в начало метода, к остальным
         int amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
         period = scanner.nextInt();
+        // тут тоже не нужен переност текста
         System.out.println(
                 "Выберите тип вклада, 1 - вклад с обычным процентом," +
                         " 2 - вклад с капитализацией:");
         action = scanner.nextInt();
+        // переменную лучше вынести в начало метода, к остальным
         double total = 0;
         if (action == 1) {
             total = calculateSimplePercent(amount, 0.06, period);
@@ -43,7 +50,7 @@ public class DepositCalculator {
                 + " лет превратятся в " + total);
     }
 
-
+    // метод мейн лучше перенести вверх и поставить первым в классе
     public static void main(String[] args) {
         new DepositCalculator().calculateTotal();
     }
